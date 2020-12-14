70ab780c661fd0db2480665818c5eda2112a6807
asd0999 <obelixotic@gmail.com>
2020-12-14 00:26:00 -0500

    show file and show link dynamic

--- client/src/components/commonView/DataTransfer.jsx
+++ client/src/components/commonView/DataTransfer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Link from "./SendLink";
+import SendLink from "./SendLink";
 import SendFile from "./SendFile";
 import { Redirect } from "react-router-dom";
 
@@ -7,33 +7,61 @@ export default class DataTransfer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sendFile: false,
-      sendLink: false,
+      showFileInput: false,
+      showLinkInput: false,
     };
     this.showLinkInput = this.showLinkInput.bind(this);
+    this.showFileInput = this.showFileInput.bind(this);
   }
 
-  showLinkInput() {}
+  showFileInput() {
+    if (this.state.showFileInput === false) {
+      this.setState({
+        showFileInput: true,
+      });
+    } else {
+      this.setState({
+        showFileInput: false,
+      });
+    }
+  }
+
+  showLinkInput() {
+    if (this.state.showLinkInput === false) {
+      this.setState({
+        showLinkInput: true,
+      });
+    } else {
+      this.setState({
+        showLinkInput: false,
+      });
+    }
+  }
 
   render() {
     return (
       <>
         {this.props.peerConnection ? (
           <>
-            <h2>Connnected to other device</h2>
-            <SendFile
-              selectFile={this.props.selectFile}
-              sendFile={this.props.sendFile}
-              resetFile={this.props.resetFile}
-            />
+            {/* <h2>Connnected to other device</h2> */}
+            <span onClick={this.showFileInput}>Share file</span>
+            {this.state.showFileInput ? (
+              <SendFile
+                selectFile={this.props.selectFile}
+                sendFile={this.props.sendFile}
+                resetFile={this.props.resetFile}
+              />
+            ) : null}
             {this.props.gotFile ? (
               <>
                 <span>You have received a file</span>
                 <button onClick={this.props.download}>Download</button>
               </>
             ) : null}
-            <span onClick={this.showLinkInput}>Share a link</span>
-            <Link sendLink={this.props.sendLink} />
+            <span onClick={this.showLinkInput}>Share link</span>
+            {this.state.showLinkInput ? (
+              <SendLink sendLink={this.props.sendLink} />
+            ) : null}
           </>
         ) : (
           <Redirect to="/" />
